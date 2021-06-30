24dfb511c89a916f33ab7ad21a906ef04397d913
Gerhard Gehrer <g.gehrer@hotmail.com>
2021-06-30 08:44:39 +0200

    Added factory method 'Create(..)' to ShareValue.

--- ShareManager/Logic/ShareValue.cs
+++ ShareManager/Logic/ShareValue.cs
@@ -12,5 +12,22 @@ namespace ShareManager.Logic
 		{
 			return $"{Name};{DateTime};{Value}";
 		}
+
+		public static ShareValue Create(string csv)
+		{
+			var result = default(ShareValue);
+			var data = csv?.Split(";");
+
+			if (data != null && data.Length == 3)
+			{
+				result = new ShareValue
+				{
+					Name = data[0],
+					DateTime = DateTime.Parse(data[1]),
+					Value = double.Parse(data[2]),
+				};
+			}
+			return result;
+		}
 	}
 }
--- ShareManager/Program.cs
+++ ShareManager/Program.cs
@@ -4,20 +4,20 @@ namespace ShareManager
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main(/*string[] args*/)
 		{
 			Console.WriteLine("ShareManager");
 
+			ConsoleWriter writer = new ();
 			Logic.Share share = new("MSFT", 100);
 			Logic.Brocker brocker = new("Max", 90.0, 110);
-			Logic.ShareHistory history = new Logic.ShareHistory("MSFT.csv");
-			ConsoleWriter writer = new ConsoleWriter();
+			Logic.ShareHistory history = new ("MSFT.csv");
 
 			share.OnValueChanged += writer.WriteShareEvent;
 			share.OnValueChanged += brocker.ChangedValueHandler;
 			share.OnValueChanged += history.ChangedValueHandler;
 			share.Start();
-			Console.ReadLine();
+			_ = Console.ReadLine();
 			share.OnValueChanged -= writer.WriteShareEvent;
 			share.OnValueChanged -= brocker.ChangedValueHandler;
 			share.OnValueChanged -= history.ChangedValueHandler;
